b524c5c750d74901bcbca1356ec1358f4ecda365
Andrey <andrey.zhelkovsky@gmail.com>
2020-11-05 17:26:33 -0500

    adding some changes to functions

--- js/script.js
+++ js/script.js
@@ -4,8 +4,53 @@ let num = 20;
 
 function showFirstMessage(text) {
     console.log(text);
-    num = 10;
+    console.log(num);
 }
 
 showFirstMessage("Hello World!");
 console.log(num);
+//  замыкание функции - сама функция + все доступные ей
+// внешние variables (переменные)
+
+function calc(a, b) {
+    return (a + b);
+}
+
+console.log(calc(4, 3));
+console.log(calc(5, 6));
+console.log(calc(10, 7));
+
+// еще одно свойство RETURN это вывод значений
+// переменной наружу например:
+
+function ret() {
+    let num = 300;
+
+    // какой-то код
+
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+// ФУНКЦИОНАЛЬНОЕ ВЫРАЖЕНИЕ - FUNCTION EXPRESSION
+
+const logger = function() {
+    console.log("Hello!");
+};
+
+logger();
+
+// МЕТОДЫ И СВОЙСТВА СТРОК И ЧИСЕЛ
+
+const str = "test";
+
+// console.log(str[2]);
+
+// МЕТОДЫ
+// например метод изменения регистра. Все методы записываются с круглыми скобками
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
