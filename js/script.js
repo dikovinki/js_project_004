75df092429e06c60452c0ad1d82fd0676d0c6ef1
Andrey <andrey.zhelkovsky@gmail.com>
2020-11-05 18:32:00 -0500

    upd JS lesson

--- js/script.js
+++ js/script.js
@@ -53,4 +53,33 @@ const str = "test";
 console.log(str.toUpperCase());
 console.log(str.toLowerCase());
 console.log(str);
+// метод поиска строки и индекса начал искомого 
+const fruit = "Some fruit";
+console.log(fruit.indexOf("fruit"));
 
+// МЕТОДЫ модификации константы строки
+
+const logg = "Hello World!";
+
+// Method .slice
+console.log(logg.slice( 0, 4));
+
+// Method .substring
+console.log(logg.substring( 0, 8));
+
+// Method .substr
+console.log(logg.substr(6, 3));
+
+// МЕТОДЫ для чисел
+// Method .round для округления до ближайшего целого числа
+
+const numb = 12.2;
+console.log(Math.round(numb));
+
+// Method .parsInt
+const test = "13.2px";
+console.log(parseInt(test));
+
+// Method .parseFloat
+const test = "13.2px";
+console.log(parseFloat(test));
